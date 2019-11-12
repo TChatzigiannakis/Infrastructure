bee4a79453e676efca5fe2bcd0ab46cb933777dd
Theodoros Chatzigiannakis <tchatzigiannakis@gmail.com>
2019-11-12 21:35:36 +0100

    Replace manifest with PowerShell scripts.

---
+++ Installers/Setup-Home.ps1
@@ -0,0 +1,40 @@
+choco install -y chocolateygui
+
+choco install -y keepass
+choco install -y dropbox
+
+choco install -y vivaldi
+choco install -y firefox
+
+choco install -y thunderbird
+choco install -y winrar
+choco install -y onedrive
+choco install -y winscp
+choco install -y wireshark
+choco install -y shutup10
+choco install -y spotify --ignore-checksums
+choco install -y vlc
+choco install -y discord
+choco install -y viber
+
+choco install -y steam
+choco install -y epicgameslauncher
+choco install -y goggalaxy
+
+choco install -y vscode
+choco install -y vscode-settingssync
+choco install -y cmake
+choco install -y Cmder
+choco install -y conan
+choco install -y git.install
+choco install -y ninja
+choco install -y nodejs
+choco install -y putty.install
+choco install -y python
+choco install -y rustup.install --ignore-checksums
+choco install -y visualstudio2019community
+choco install -y visualstudio2019buildtools
+choco install -y visualstudio2019-workload-visualstudioextension
+choco install -y visualstudio2019-workload-manageddesktop
+choco install -y visualstudio2019-workload-netcoretools
+choco install -y visualstudio2019teamexplorer
