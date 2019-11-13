9c54912c3aa5434415dad188fbe2607770b3566d
Theodoros Chatzigiannakis <tchatzigiannakis@gmail.com>
2019-11-13 11:19:57 +0100

    Add more stuff to PowerShell script.

--- Installers/Setup-Home.ps1
+++ Installers/Setup-Home.ps1
@@ -1,40 +1,21 @@
-choco install -y chocolateygui
+# Essentials
+"keepass", "dropbox", "chocolateygui" | ForEach-Object -Process {choco install -y $_}
 
-choco install -y keepass
-choco install -y dropbox
+# Browsers
+"vivaldi", "firefox", "googlechrome" | ForEach-Object -Process {choco install -y $_}
 
-choco install -y vivaldi
-choco install -y firefox
+# Common
+"thunderbird", "winrar", "onedrive", "vlc", "discord", "viber", "signal", "skype", "whatsapp", "twitch", "adobereader", "office365proplus", "calibre", "f.lux", "sharex" | ForEach-Object -Process {choco install -y $_}
+"spotify" | ForEach-Object -Process {choco install -y $_ --ignore-checksums}
 
-choco install -y thunderbird
-choco install -y winrar
-choco install -y onedrive
-choco install -y winscp
-choco install -y wireshark
-choco install -y shutup10
-choco install -y spotify --ignore-checksums
-choco install -y vlc
-choco install -y discord
-choco install -y viber
+# Advanced
+"paint.net", "pandoc", "putty.install", "winscp", "ffmpeg", "obs-studio", "rufus", "speccy", "nmap", "microsoft-windows-terminal", "cmder", "hugo", "sysinternals", "autoruns", "shutup10", "wireshark", "vmware-workstation-player", "heidisql" | ForEach-Object -Process {choco install -y $_}
 
-choco install -y steam
-choco install -y epicgameslauncher
-choco install -y goggalaxy
+# Gaming
+"goggalaxy", "steam", "epicgameslauncher", "uplay", "dolphin" | ForEach-Object -Process {choco install -y $_}
 
-choco install -y vscode
-choco install -y vscode-settingssync
-choco install -y cmake
-choco install -y Cmder
-choco install -y conan
-choco install -y git.install
-choco install -y ninja
-choco install -y nodejs
-choco install -y putty.install
-choco install -y python
-choco install -y rustup.install --ignore-checksums
-choco install -y visualstudio2019community
-choco install -y visualstudio2019buildtools
-choco install -y visualstudio2019-workload-visualstudioextension
-choco install -y visualstudio2019-workload-manageddesktop
-choco install -y visualstudio2019-workload-netcoretools
-choco install -y visualstudio2019teamexplorer
+# Development
+"vscode", "vscode-settingssync" | ForEach-Object -Process {choco install -y $_}
+"cmake", "conan", "firacode", "ghc", "cabal", "git.install", "intellijidea-community", "jdk8", "llvm", "linqpad", "ninja", "nodejs", "paket", "python", "ruby" | ForEach-Object -Process {choco install -y $_}
+"visualstudio2019community", "visualstudio2019buildtools", "visualstudio2019-workload-visualstudioextension", "visualstudio2019-workload-manageddesktop", "visualstudio2019-workload-netcoretools", "visualstudio2019teamexplorer" | ForEach-Object -Process {choco install -y $_}
+"rustup.install" | ForEach-Object -Process {choco install -y $_ --ignore-checksums}
